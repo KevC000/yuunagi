33ceb7c59bdff077662e932a8369135eccee99be
KevC000 <chenkevin1119@gmail.com>
2023-08-09 00:42:43 -0400

    getx global search query state

--- lib/util/app_controller.dart
+++ lib/util/app_controller.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 
 class AppController extends GetxController {
-  String dictionarySearchQuery = 'test';
+  String dictionarySearchQuery = '';
 
   void setDictionarySearchQuery(String query) {
     dictionarySearchQuery = query;
